749a01111995ae677b81385729cd7b0f0ee3b2f6
Iveth89 <79477684+Iveth89@users.noreply.github.com>
2021-12-12 14:27:22 -0600

    Add files via upload

---
+++ public/js/detalle.js
@@ -0,0 +1,28 @@
+//var searchParams = new URLSearchParams(window.location.search);
+  var  pr_id = sessionStorage.getItem('pr_id');
+var urlApi = 'https://delicase.vercel.app';
+
+function cargaProducto(pr_id = 0) {
+var cont=0;
+    fetch(urlApi + '/general/getProducto?pr_id='+pr_id).then(response => response.json()).then(function (data) {
+        document.getElementById('imagen').src='/assets/imagenes/'+ data.pr_imagen;
+        document.getElementById('titulo').innerHTML= data.pr_nombre;
+        document.getElementById('descripcion').innerHTML= data.pr_descripcion 
+
+        document.getElementById('agregar').addEventListener('click',function(){
+
+            agregaProductoCarrito(data);
+        });
+    }).catch((e)=>{
+                if(pr_id==null){
+                 window.location.href=window.location.origin;
+                }
+                if(cont<3)
+                 window.location.reload();
+      
+     		        	bootbox.alert("Algo salio mal al cargar el detalle verifique su conexion e intentelo nuevamente");
+    });
+}
+ window.addEventListener("load", function() {
+cargaProducto(pr_id);
+ });
