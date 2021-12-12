37a796cb964e23beefd84714cbf52fbc7cd57f50
Iveth89 <79477684+Iveth89@users.noreply.github.com>
2021-12-12 14:57:22 -0600

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -60,16 +60,29 @@ self.addEventListener("activate", (event) => {
 
 
 
-self.addEventListener('fetch', function(event) {
+addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open(dynamicCache).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(dynamicCache)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(dynamicCache)
+                .then(function(cache) {
+                  return cache.match('/index.html');
+                });
+            });
+        }
+      })
   );
 });
 
